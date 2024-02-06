51a41b268248a3e16e84df365f5974fe4a0dbd0d
kishorekhn <115566952+kishorekhn@users.noreply.github.com>
2024-02-06 22:24:40 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+from flask import flask,render_template
+
+app=flask(_name_)
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+if _name_="_main_":
+    app.run(debug=false,host='0.0.0.')
